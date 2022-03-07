4d6771d4702d5c77733332caac5439d20e7f8a6f
Aditi Puntambekar <apuntamb@redhat.com>
2022-03-07 18:56:16 +0530

    Define Status API (#2)
    * Define Status API
    
    * Add OpenAPI spec for status endpoint
    
    * Align openapi spec with older approach

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello from Yuptoo!"
-
-app.run(host='0.0.0.0', port=8080)
---
+++ yuptoo/api/v1/status.py
@@ -0,0 +1,7 @@
+from flask_restful import Resource
+                                  
+
+class Status(Resource):
+    def get(self):
+        return {'status': 'Application is running!'}
+
