c15740fa0bff74ff12647c77ec80d6f96584f723
Suraj Patil <patilsuraj767@gmail.com>
2022-07-15 12:17:03 +0530

    [YUPTOO-23] Add Kafka auth config

--- yuptoo/lib/produce.py
+++ yuptoo/lib/produce.py
@@ -1,8 +1,13 @@
 from confluent_kafka import Producer
-from yuptoo.lib.config import INSIGHTS_KAFKA_ADDRESS, KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE
+from yuptoo.lib.config import INSIGHTS_KAFKA_ADDRESS, KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE, kafka_auth_config
 
 
 def init_producer():
+    connection_object = {
+        'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
+        'message.max.bytes': KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE
+    }
+    kafka_auth_config(connection_object)
     producer = Producer({
         'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
         'message.max.bytes': KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE
