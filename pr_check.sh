10b24f19261ee2a1ef97d6b5c1affeaea6b0eea8
Sanket Jagtap <san7ketjagtap@gmail.com>
2022-08-02 18:25:12 +0530

    Update IQE plugin name

--- pr_check.sh
+++ pr_check.sh
@@ -5,7 +5,7 @@ export COMPONENT_NAME="yuptoo"                # name of app-sre "resourceTemplat
 export IMAGE="quay.io/cloudservices/yuptoo"   # the image location on quay
 export DOCKERFILE="Dockerfile"
 
-export IQE_PLUGINS="foreman-rh-cloud"         # name of the IQE plugin for this APP
+export IQE_PLUGINS="foreman_rh_cloud"         # name of the IQE plugin for this APP
 export IQE_MARKER_EXPRESSION="yupana_smoke"   # This is the value passed to pytest -m
 export IQE_FILTER_EXPRESSION=""               # This is the value passed to pytest -k
 export IQE_CJI_TIMEOUT="30m"                  # This is the time to wait for smoke test to complete or fail
