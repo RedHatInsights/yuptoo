89ed8d471d0757e521589eedf06ae805c1b8befd
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2022-08-03 17:07:07 +0530

    [YUPTOO-23] Add Kafka auth config (#14)

--- yuptoo/lib/config.py
+++ yuptoo/lib/config.py
@@ -5,10 +5,26 @@ LOG = logging.getLogger(__name__)
 
 CLOWDER_ENABLED = True if os.getenv("CLOWDER_ENABLED", default="False").lower() in ["true", "t", "yes", "y"] else False
 
+
+def kafka_auth_config(connection_object):
+    if KAFKA_BROKER:
+        if KAFKA_BROKER.cacert:
+            connection_object["ssl.ca.location"] = KAFKA_BROKER.cacert
+        if KAFKA_BROKER.sasl and KAFKA_BROKER.sasl.username:
+            connection_object.update({
+                "security.protocol": KAFKA_BROKER.sasl.securityProtocol,
+                "sasl.mechanisms": KAFKA_BROKER.sasl.saslMechanism,
+                "sasl.username": KAFKA_BROKER.sasl.username,
+                "sasl.password": KAFKA_BROKER.sasl.password,
+            })
+    return connection_object
+
+
 if CLOWDER_ENABLED:
     LOG.info("Using Clowder Operator...")
     from app_common_python import LoadedConfig, KafkaTopics
-    INSIGHTS_KAFKA_ADDRESS = LoadedConfig.kafka.brokers[0].hostname + ":" + str(LoadedConfig.kafka.brokers[0].port)
+    KAFKA_BROKER = LoadedConfig.kafka.brokers[0]
+    INSIGHTS_KAFKA_ADDRESS = KAFKA_BROKER.hostname + ":" + str(KAFKA_BROKER.port)
     QPC_TOPIC = KafkaTopics["platform.upload.qpc"].name
     UPLOAD_TOPIC = KafkaTopics["platform.inventory.host-ingress"].name
     VALIDATION_TOPIC = KafkaTopics["platform.upload.validation"].name
@@ -16,6 +32,7 @@ else:
     INSIGHTS_KAFKA_HOST = os.getenv('INSIGHTS_KAFKA_HOST', 'localhost')
     INSIGHTS_KAFKA_PORT = os.getenv('INSIGHTS_KAFKA_PORT', '29092')
     INSIGHTS_KAFKA_ADDRESS = f'{INSIGHTS_KAFKA_HOST}:{INSIGHTS_KAFKA_PORT}'
+    KAFKA_BROKER = None
     QPC_TOPIC = os.getenv('QPC_TOPIC', 'platform.upload.qpc')
     VALIDATION_TOPIC = os.getenv('VALIDATION_TOPIC', 'platform.upload.validation')
     UPLOAD_TOPIC = os.getenv('UPLOAD_TOPIC', 'platform.inventory.host-ingress')
--- yuptoo/lib/consume.py
+++ yuptoo/lib/consume.py
@@ -1,14 +1,16 @@
 from confluent_kafka import Consumer
-from yuptoo.lib.config import KAFKA_AUTO_COMMIT, INSIGHTS_KAFKA_ADDRESS, QPC_TOPIC
+from yuptoo.lib.config import KAFKA_AUTO_COMMIT, INSIGHTS_KAFKA_ADDRESS, QPC_TOPIC, kafka_auth_config
 
 
 def init_consumer():
-    consumer = Consumer({
-                'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
-                'group.id': 'qpc-group',
-                'auto.offset.reset': 'earliest',
-                'enable.auto.commit': KAFKA_AUTO_COMMIT
-                })
+    connection_object = {
+        'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
+        'group.id': 'qpc-group',
+        'auto.offset.reset': 'earliest',
+        'enable.auto.commit': KAFKA_AUTO_COMMIT
+    }
+    kafka_auth_config(connection_object)
+    consumer = Consumer(connection_object)
     consumer.subscribe([QPC_TOPIC])
 
     return consumer
