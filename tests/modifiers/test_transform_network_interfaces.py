a804c6acac40db79ad710117787e4eb86cb16b2b
kgaikwad <kavitagaikwad103@gmail.com>
2022-09-06 00:09:19 +0530

    Fixes #YUPTOO-37 - omit nics starts with & associated mac_addrs

--- tests/modifiers/test_transform_network_interfaces.py
+++ tests/modifiers/test_transform_network_interfaces.py
@@ -120,3 +120,44 @@ def test_do_not_run_mtu_transformation_when_none():
             }
         }
     assert host == result
+
+
+def test_omit_nics_starts_with_cali():
+    """Test omit nics starts with cali from host."""
+    host = {
+            'mac_addresses': ['value', 'test'],
+            'system_profile': {
+                'network_interfaces': [
+                    {'ipv4_addresses': ['192.168.10.10'], 'ipv6_addresses': [],
+                     'mac_address': 'value', 'name': 'cali916c8937279'},
+                    {'ipv4_addresses': ['192.168.10.11'], 'ipv6_addresses': [],
+                     'mac_address': 'test', 'name': 'testcali'},
+                ]
+            }}
+    transformed_obj = {'removed': [], 'modified': [], 'missing_data': []}
+    TransformNetworkInterfaces().run(host, transformed_obj)
+    result = {
+        'mac_addresses': ['test'],
+        'system_profile': {
+            'network_interfaces': [
+                {'ipv4_addresses': ['192.168.10.11'],
+                 'ipv6_addresses': [],
+                 'mac_address': 'test', 'name': 'testcali'}
+            ]}}
+    assert host == result
+
+
+def test_remove_empty_mac_addresses_while_omitting_nics():
+    """Test remove mac addrs for omitted nics,remove empty mac_addrs."""
+    host = {
+            'mac_addresses': ['value'],
+            'system_profile': {
+                'network_interfaces': [
+                    {'ipv4_addresses': ['192.168.10.10'], 'ipv6_addresses': [],
+                     'mac_address': 'value', 'name': 'cali916c8937279'}
+                ]
+            }}
+    transformed_obj = {'removed': [], 'modified': [], 'missing_data': []}
+    TransformNetworkInterfaces().run(host, transformed_obj)
+    result = {'system_profile': {'network_interfaces': []}}
+    assert host == result
