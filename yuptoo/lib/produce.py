af8c93e493283df1d2898c9506a3563f61d62e98
Suraj Patil <patilsuraj767@gmail.com>
2022-09-07 14:08:48 +0530

    Refs #YUPTOO-23 - Pass connection object to producer

--- yuptoo/lib/produce.py
+++ yuptoo/lib/produce.py
@@ -8,9 +8,6 @@ def init_producer():
         'message.max.bytes': KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE
     }
     kafka_auth_config(connection_object)
-    producer = Producer({
-        'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
-        'message.max.bytes': KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE
-    })
+    producer = Producer(connection_object)
 
     return producer
