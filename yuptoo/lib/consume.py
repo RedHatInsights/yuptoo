4337328d0f3d21a4501257fa15d0c1d6942e0e6c
Suraj Patil <patilsuraj767@gmail.com>
2022-09-12 14:41:54 +0530

    Fixes #YUPTOO-39 - Make consumer group configurable via environment variable

--- yuptoo/lib/consume.py
+++ yuptoo/lib/consume.py
@@ -1,12 +1,17 @@
 from confluent_kafka import Consumer
-from yuptoo.lib.config import KAFKA_AUTO_COMMIT, INSIGHTS_KAFKA_ADDRESS, ANNOUNCE_TOPIC, kafka_auth_config
+from yuptoo.lib.config import (
+        KAFKA_AUTO_COMMIT,
+        INSIGHTS_KAFKA_ADDRESS,
+        ANNOUNCE_TOPIC,
+        KAFKA_CONSUMER_GROUP_ID,
+        kafka_auth_config
+    )
 
 
 def init_consumer():
     connection_object = {
         'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
-        'group.id': 'qpc-group',
-        'auto.offset.reset': 'earliest',
+        'group.id': KAFKA_CONSUMER_GROUP_ID,
         'enable.auto.commit': KAFKA_AUTO_COMMIT
     }
     kafka_auth_config(connection_object)
