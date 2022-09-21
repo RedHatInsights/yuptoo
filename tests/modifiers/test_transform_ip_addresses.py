5f123f959e109e9425b0f51db98c92749bfb398e
kgaikwad <kavitagaikwad103@gmail.com>
2022-09-21 19:37:10 +0530

    Fixes #YUPTOO-21 - remove duplicate entries from ip_addresses

--- tests/modifiers/test_transform_ip_addresses.py
+++ tests/modifiers/test_transform_ip_addresses.py
@@ -1,12 +1,12 @@
-from yuptoo.modifiers.remove_empty_ip_addresses import RemoveEmptyIpAddress
+from yuptoo.modifiers.transform_ip_addresses import TransformIPAddress
 
 
 def test_remove_empty_ip_addresses():
     """Test remove host ip_addresses."""
     host = {'ip_addresses': []}
     transformed_obj = {'removed': []}
-    RemoveEmptyIpAddress().run(host, transformed_obj)
-    assert host == {}
+    TransformIPAddress().run(host, transformed_obj)
+    assert not host
     assert 'empty ip_addresses' in transformed_obj['removed']
 
 
@@ -15,7 +15,7 @@ def test_do_not_remove_set_ip_addresses():
     host = {
         'ip_addresses': ['192.168.10.10']}
     transformed_obj = {'removed': []}
-    RemoveEmptyIpAddress().run(host, transformed_obj)
+    TransformIPAddress().run(host, transformed_obj)
     assert host == {'ip_addresses': ['192.168.10.10']}
     assert len(transformed_obj['removed']) == 0
 
@@ -24,6 +24,16 @@ def test_ip_addresses_field():
     """Test remove both empty ip and mac addresses."""
     host = {}
     transformed_obj = {'removed': []}
-    RemoveEmptyIpAddress().run(host, transformed_obj)
+    TransformIPAddress().run(host, transformed_obj)
     assert len(transformed_obj['removed']) == 0
-    assert host == {}
+    assert not host
+
+
+def test_remove_duplicate_ip_addresses():
+    """Test remove duplicate entries & make host ip_addresses unique."""
+    host = {
+        'ip_addresses': ['192.168.10.10', '192.168.10.10']}
+    transformed_obj = {'modified': []}
+    TransformIPAddress().run(host, transformed_obj)
+    assert host == {'ip_addresses': ['192.168.10.10']}
+    assert transformed_obj['modified']
