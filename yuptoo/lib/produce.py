6210b4ddb3b584b1f5f4d07f186024cf2783d504
Suraj Patil <patilsuraj767@gmail.com>
2022-09-30 09:56:15 +0530

    Refs #YUPTOO-47 - Fix host counter issue

--- yuptoo/lib/produce.py
+++ yuptoo/lib/produce.py
@@ -1,5 +1,8 @@
 from confluent_kafka import Producer, KafkaException
-from yuptoo.lib.config import INSIGHTS_KAFKA_ADDRESS, KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE, kafka_auth_config
+from yuptoo.lib.config import (INSIGHTS_KAFKA_ADDRESS, KAFKA_PRODUCER_OVERRIDE_MAX_REQUEST_SIZE,
+                               kafka_auth_config, UPLOAD_TOPIC)
+from functools import partial
+from yuptoo.lib.metrics import host_uploaded, host_upload_failures
 import logging
 import json
 
@@ -18,22 +21,30 @@ def init_producer():
     return producer
 
 
-def send_message(kafka_topic, msg):
+def send_message(kafka_topic, msg, request_obj=None):
 
-    msg_sent = False
-
-    def delivery_report(err, msg=None):
+    def delivery_report(err, msg=None, is_msg_for_hbi=False):
+        nonlocal request_obj
         if err is not None:
             LOG.error(f"Message delivery for topic {msg.topic()} failed: {err}")
+            if is_msg_for_hbi:
+                host_upload_failures.labels(
+                        org_id=request_obj['org_id']
+                    ).inc()
         else:
-            nonlocal msg_sent
-            msg_sent = True
+            if is_msg_for_hbi:
+                request_obj['host_inventory_upload_count'] += 1
+                host_uploaded.labels(
+                        org_id=request_obj['org_id']
+                    ).inc()
             LOG.debug(f"Message delivered to {msg.topic()} [{msg.partition()}]")
 
     try:
         bytes = json.dumps(msg, ensure_ascii=False).encode("utf-8")
-        producer.produce(kafka_topic, bytes, callback=delivery_report)
+        if kafka_topic == UPLOAD_TOPIC:
+            producer.produce(kafka_topic, bytes, callback=partial(delivery_report, is_msg_for_hbi=True))
+        else:
+            producer.produce(kafka_topic, bytes, callback=delivery_report)
         producer.poll(1)
-        return msg_sent
     except KafkaException:
         LOG.error(f"Failed to produce message to [{kafka_topic}] topic.")
