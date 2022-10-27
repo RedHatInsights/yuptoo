98dee105ac217ec673c46fc42a88d908599e2127
Sanket Jagtap <san7ketjagtap@gmail.com>
2022-10-27 14:21:22 +0530

    Fixes #YUPTOO-53 - Turncate the value for tags with more than 255 char

--- tests/modifiers/test_transform_tags.py
+++ tests/modifiers/test_transform_tags.py
@@ -44,3 +44,50 @@ def test_transform_tags_value_to_string():
 
     assert host == result
     assert 'tags' in transformed_obj['modified']
+
+
+def test_transform_tags_255_char():
+    """Test tags transformation for host for more than 255 char"""
+    host = {'tags': [
+        {
+            'namespace': 'satellite_parameter',
+            'key': 'host_registration_insights',
+            'value': "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+                     "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+                     "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+        },
+        {
+            'namespace': 'satellite_parameter',
+            'key': 'host_registration_remote_execution',
+            'value': False
+        },
+        {
+            'namespace': 'satellite',
+            'key': 'organization_id',
+            'value': 1
+        }
+    ]}
+
+    transformed_obj = {'removed': [], 'modified': [], 'missing_data': []}
+    TransformTags().run(host, transformed_obj)
+
+    result = {'tags': [
+            {
+                'namespace': 'satellite_parameter',
+                'key': 'host_registration_insights',
+                'value': 'Truncated.Longer than maximum length 255.'
+            },
+            {
+                'namespace': 'satellite_parameter',
+                'key': 'host_registration_remote_execution',
+                'value': 'false'
+            },
+            {
+                'namespace': 'satellite',
+                'key': 'organization_id',
+                'value': '1'
+            }
+        ]}
+
+    assert host == result
+    assert 'tags' in transformed_obj['modified']
