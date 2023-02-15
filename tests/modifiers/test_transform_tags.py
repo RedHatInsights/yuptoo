e79d32e9de0d555cdc0233007632cd5a7984b727
kgaikwad <kavitagaikwad103@gmail.com>
2023-02-15 16:30:38 +0530

    Fixes #YUPTOO-53 - Truncate value for tags with more than 250 char (#53)
    * Fixes #YUPTOO-53 - Truncate value for tags with more than 250 char
    
    ---------
    
    Co-authored-by: Sanket Jagtap <san7ketjagtap@gmail.com>

--- tests/modifiers/test_transform_tags.py
+++ tests/modifiers/test_transform_tags.py
@@ -44,3 +44,50 @@ def test_transform_tags_value_to_string():
 
     assert host == result
     assert 'tags' in transformed_obj['modified']
+
+
+def test_transform_tags_250_char():
+    """Test tags transformation for host for more than 250 char"""
+    host = {'tags': [
+        {
+            'namespace': 'satellite_parameter',
+            'key': 'host_registration_insights',
+            'value': "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+                     "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+                     "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+        },
+        {
+            'namespace': 'satellite_parameter',
+            'key': 'host_registration_remote_execution',
+            'value': False
+        },
+        {
+            'namespace': 'satellite',
+            'key': 'organization_id',
+            'value': 1
+        }
+    ]}
+
+    transformed_obj = {'removed': [], 'modified': [], 'missing_data': []}
+    TransformTags().run(host, transformed_obj)
+
+    result = {'tags': [
+            {
+                'namespace': 'satellite_parameter',
+                'key': 'host_registration_insights',
+                'value': 'Original value exceeds 250 characters.'
+            },
+            {
+                'namespace': 'satellite_parameter',
+                'key': 'host_registration_remote_execution',
+                'value': 'false'
+            },
+            {
+                'namespace': 'satellite',
+                'key': 'organization_id',
+                'value': '1'
+            }
+        ]}
+
+    assert host == result
+    assert 'tags' in transformed_obj['modified']
--- yuptoo/modifiers/transform_tags.py
+++ yuptoo/modifiers/transform_tags.py
@@ -7,6 +7,11 @@ class TransformTags(Modifier):
         if tags:
             tags_modified = False
             for tag in tags:
+
+                if len(str(tag['value'])) > 250:
+                    tag['value'] = "Original value exceeds 250 characters."
+                    tags_modified = True
+
                 if tag['value'] is None or isinstance(tag['value'], str):
                     continue
 
@@ -16,7 +21,6 @@ class TransformTags(Modifier):
                     tag['value'] = 'false'
                 else:
                     tag['value'] = str(tag['value'])
-
                 tags_modified = True
 
             if tags_modified:
