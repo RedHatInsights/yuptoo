b06644f1cafdaf05e93f209c069081ff8bb5afa1
Chris Hambridge <chambrid@redhat.com>
2025-06-25 09:58:02 -0400

    feat: Add pbr>=1.8 to add_manual_build_dependencies.sh

--- .hermetic_builds/add_manual_build_dependencies.sh
+++ .hermetic_builds/add_manual_build_dependencies.sh
@@ -7,3 +7,4 @@ echo "calver>=2022.6.26" >> requirements-build.in
 echo "mypy<=1.15.0,>=1.4.1" >> requirements-build.in
 echo "types-psutil>=6.0.0.20240621" >> requirements-build.in
 echo "types-setuptools>=75.6.0.20241223" >> requirements-build.in
+echo "pbr>=1.8" >> requirements-build.in
