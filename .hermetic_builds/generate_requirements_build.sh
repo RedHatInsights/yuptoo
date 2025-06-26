884c14f90a94490f8860418716d4556fa39aa6fb
Chris Hambridge <chambrid@redhat.com>
2025-06-26 15:24:45 -0400

    feat: Add requirements-extras.txt

--- .hermetic_builds/generate_requirements_build.sh
+++ .hermetic_builds/generate_requirements_build.sh
@@ -5,3 +5,4 @@ pip3 install "pip<25"
 cd /var/tmp
 
 pybuild-deps compile --generate-hashes requirements.txt -o requirements-build.txt
+pip-compile requirements-build.in --allow-unsafe --generate-hashes -o requirements-extras.txt
