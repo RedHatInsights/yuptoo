efec8aaa2f889cb44ad4c0e80e3b56f6b3cd42f2
Chris Hambridge <chambrid@redhat.com>
2025-06-27 10:25:31 -0400

    feat: Include python3.11-devel package

--- .hermetic_builds/prep_python_build_container_dependencies.sh
+++ .hermetic_builds/prep_python_build_container_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-microdnf install --setopt=tsflags=nodocs -y python3.11 python3.11-pip which gcc && \
+microdnf install --setopt=tsflags=nodocs -y python3.11 python3.11-pip python3.11-devel which gcc && \
     microdnf upgrade -y && \
     microdnf clean all
 set -ex && if [ -e `which python3.11` ]; then ln -s `which python3.11` /usr/local/bin/python3; fi
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM registry.access.redhat.com/ubi9/ubi-minimal:latest
 
-RUN microdnf install --setopt=tsflags=nodocs -y python3.11 python3.11-pip which gcc && \
+RUN microdnf install --setopt=tsflags=nodocs -y python3.11 python3.11-pip python3.11-devel which gcc && \
     microdnf upgrade -y && \
     microdnf clean all
 
