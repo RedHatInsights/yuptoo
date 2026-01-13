343bfa7ee6be5d1d6c8f04c89f8e49b8d12a05d2
Xiaoxue Wang <xiaoxwan@redhat.com>
2026-01-13 11:02:44 +0800

    fix(deps): refresh requirements on Pipfile.lock change
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- .hermetic_builds/generate_requirements_build.sh
+++ .hermetic_builds/generate_requirements_build.sh
@@ -4,5 +4,5 @@ pip3 install pip-tools pybuild-deps
 pip3 install "pip<25"
 cd /var/tmp
 
-pybuild-deps compile --generate-hashes requirements.txt -o requirements-build.txt
-pip-compile requirements-build.in --allow-unsafe --generate-hashes -o requirements-extras.txt
+pybuild-deps compile --generate-hashes requirements.txt -o requirements-build.txt --verbose
+pip-compile requirements-build.in --allow-unsafe --generate-hashes -o requirements-extras.txt --verbose
