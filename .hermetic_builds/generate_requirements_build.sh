02082f6e2c25660bf7c9ed80c1fea2aaf6021db8
Ping Qin <piqin@redhat.com>
2026-06-25 20:48:43 +0900

    fix: Violation code - sbom_spdx.disallowed_package_attributes
    - When downloding binary files, the following error will be shown:
      * [Violation] sbom_spdx.disallowed_package_attributes
    - This commit try to not download binary files and fix the PIP_BUILD_CONSTRAINT
      for setuptools<82

--- .hermetic_builds/generate_requirements_build.sh
+++ .hermetic_builds/generate_requirements_build.sh
@@ -5,4 +5,17 @@ pip3 install "pip<25"
 cd /var/tmp
 
 pybuild-deps compile --generate-hashes requirements.txt -o requirements-build.txt
+
+# pybuild-deps may emit setuptools==82 even when requirements.txt pins <82.
+if grep -q '^setuptools==82\.' requirements-build.txt; then
+    # Drop the setuptools==82 pin block (package line + indented continuations).
+    awk '
+      /^setuptools==82\./ { skip=1; next }
+      skip && /^[[:space:]]/ { next }
+      skip { skip=0 }
+      { print }
+    ' requirements-build.txt > requirements-build.txt.tmp \
+      && mv requirements-build.txt.tmp requirements-build.txt
+fi
+
 pip-compile requirements-build.in --allow-unsafe --generate-hashes -o requirements-extras.txt
